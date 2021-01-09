f4534bc0f86cf9e4c9696603f5be787af48c8087
Sierra Wallace <sskwallace@gmail.com>
2021-01-09 02:59:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
     create_table :shows do |t|
       t.string :name 
       t.string :network 
-      t.sting :day 
+      t.string :day 
       t.integer :rating 
     end
   end
